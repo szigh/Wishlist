f8b12149374d85ef6ca93a93d8109fe68406587b
szigh <148691844+szigh@users.noreply.github.com>
2025-11-23 10:16:40 +0000

    other controllers

---
+++ WishlistWeb/Controllers/UsersController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly WishlistDbContext _context;
+
+    public UsersController(WishlistDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/users
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    // GET: api/users/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null) return NotFound();
+        return user;
+    }
+
+    // POST: api/users
+    [HttpPost]
+    public async Task<ActionResult<User>> PostUser(User user)
+    {
+        // TODO: hash password before saving
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+    }
+
+    // PUT: api/users/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutUser(int id, User user)
+    {
+        if (id != user.Id) return BadRequest();
+        _context.Entry(user).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    // DELETE: api/users/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null) return NotFound();
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ WishlistWeb/Controllers/VolunteersController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api/[controller]")]
+public class VolunteersController : ControllerBase
+{
+    private readonly WishlistDbContext _context;
+
+    public VolunteersController(WishlistDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/volunteers
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Volunteer>>> GetVolunteers()
+    {
+        return await _context.Volunteers
+            .Include(v => v.Gift)
+            .Include(v => v.VolunteerUser)
+            .ToListAsync();
+    }
+
+    // GET: api/volunteers/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Volunteer>> GetVolunteer(int id)
+    {
+        var volunteer = await _context.Volunteers
+            .Include(v => v.Gift)
+            .Include(v => v.VolunteerUser)
+            .FirstOrDefaultAsync(v => v.Id == id);
+
+        if (volunteer == null) return NotFound();
+        return volunteer;
+    }
+
+    // POST: api/volunteers
+    [HttpPost]
+    public async Task<ActionResult<Volunteer>> PostVolunteer(Volunteer volunteer)
+    {
+        _context.Volunteers.Add(volunteer);
+
+        // Mark gift as taken
+        var gift = await _context.Gifts.FindAsync(volunteer.GiftId);
+        if (gift != null) gift.IsTaken = true;
+
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetVolunteer), new { id = volunteer.Id }, volunteer);
+    }
+
+    // DELETE: api/volunteers/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteVolunteer(int id)
+    {
+        var volunteer = await _context.Volunteers.FindAsync(id);
+        if (volunteer == null) return NotFound();
+
+        _context.Volunteers.Remove(volunteer);
+
+        // Reset gift status if needed
+        var gift = await _context.Gifts.FindAsync(volunteer.GiftId);
+        if (gift != null) gift.IsTaken = false;
+
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
