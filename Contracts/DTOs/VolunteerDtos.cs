70dacbdfc2e7f6471e97eb4a2513ff78afa9881b
szigh <148691844+szigh@users.noreply.github.com>
2025-11-23 13:47:46 +0000

    Add DTOs

---
+++ Contracts/DTOs/VolunteerDtos.cs
@@ -0,0 +1,17 @@
+﻿namespace Contracts.DTOs
+{
+    // For volunteering for a gift
+    public class VolunteerCreateDto
+    {
+        public int GiftId { get; set; }
+        public int VolunteerUserId { get; set; }
+    }
+
+    // For returning volunteer info
+    public class VolunteerReadDto
+    {
+        public int Id { get; set; }
+        public int GiftId { get; set; }
+        public int VolunteerUserId { get; set; }
+    }
+}
