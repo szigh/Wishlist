b91364dabf7c65ac1178b9406c23620da754a5f3
szigh <148691844+szigh@users.noreply.github.com>
2025-11-23 13:52:29 +0000

    Better controllers; using Mappers and DTOs

---
+++ WishlistWeb/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Contracts.DTOs;
+using Models;
+
+namespace WishlistWeb
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // User mappings
+            CreateMap<User, UserReadDto>();
+            CreateMap<UserCreateDto, User>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore()); // handled manually
+            CreateMap<UserUpdateDto, User>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore()) // never overwrite password
+                .ForMember(dest => dest.Id, opt => opt.Ignore());          // Id comes from route, not DTO
+
+            // Gift mappings
+            CreateMap<Gift, GiftReadDto>();
+            CreateMap<GiftCreateDto, Gift>();
+            CreateMap<GiftUpdateDto, Gift>();
+
+            // Volunteer mappings
+            CreateMap<Volunteer, VolunteerReadDto>();
+            CreateMap<VolunteerCreateDto, Volunteer>();
+        }
+    }
+}
--- WishlistWeb/Program.cs
+++ WishlistWeb/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using Models;
+using WishlistWeb;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAutoMapper(cfg => {
+    cfg.LicenseKey = builder.Configuration.GetValue<string>("AutoMapperLicenseKey");
+}, typeof(MappingProfile));
 builder.Services.AddDbContext<WishlistDbContext>(
-    options => options.UseSqlite(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers(); 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
